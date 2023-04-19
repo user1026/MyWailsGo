4bba6a16f7bf5eb30a9e643f94d7e8be11a7d0ec
user1026 <2914153618@qq.com>
2023-04-19 15:27:16 +0800

    123

--- AppMenu/FileMenu.go
+++ AppMenu/FileMenu.go
@@ -7,6 +7,7 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/menu"
 	"github.com/wailsapp/wails/v2/pkg/menu/keys"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
+	"os"
 )
 
 var event = Event.EventList
@@ -18,22 +19,34 @@ func AddFileMnuList(menu *menu.Menu) {
 
 func openFile(_ *menu.CallbackData) {
 	fmt.Println(Global.Global_ConText)
-	file, err := runtime.OpenFileDialog(Global.Global_ConText, runtime.OpenDialogOptions{
+	fileName, err := runtime.OpenFileDialog(Global.Global_ConText, runtime.OpenDialogOptions{
 		DefaultDirectory: "C:",
 		Title:            "请选择文件",
 	})
-	if err != nil {
-		runtime.MessageDialog(Global.Global_ConText, runtime.MessageDialogOptions{
-			Type:          "error",
-			Title:         "发生错误",
-			Message:       "选择文件时发生错误",
-			DefaultButton: "YES",
-		})
+	if Global.ErrorDialog(err, "选择文件时发生错误") {
+		return
+	}
+	//file, openErr := excelize.OpenFile()
+	file, openErr := os.Open(fileName)
+	if Global.ErrorDialog(openErr, "打开文件时发生错误") {
+		return
 	}
 	fmt.Println(file)
-	if file != "" {
+	fileInfo, er := file.Stat()
+	if Global.ErrorDialog(er, "获取文件信息时发生错误") {
+		return
+	}
+	var fileData = make([]byte, fileInfo.Size())
+	_, readerr := file.Read(fileData)
+	if Global.ErrorDialog(readerr, "读取文件信息时发生错误") {
+		return
+	}
+	fmt.Println(string(fileData))
+	if file != nil {
 		event.GotoJs("openFile", file)
 	}
-
+	defer func() {
+		file.Close()
+	}()
 	//runtime.EventsEmit(Global.Global_ConText, "openFile", "123")
 }
--- Global/global.go
+++ Global/global.go
@@ -1,6 +1,9 @@
 package Global
 
-import "context"
+import (
+	"context"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
 
 var (
 	Global_ConText context.Context
@@ -8,4 +11,26 @@ var (
 
 type Global struct {
 	Context context.Context
+	Error   error
+}
+
+func ErrorDialog(err error, str string) bool {
+	if err != nil {
+		runtime.MessageDialog(Global_ConText, runtime.MessageDialogOptions{
+			Type:          "error",
+			Title:         "发生错误",
+			Message:       str,
+			DefaultButton: "是",
+		})
+		return true
+	}
+	return false
+}
+func InfoDialog(str string) {
+	runtime.MessageDialog(Global_ConText, runtime.MessageDialogOptions{
+		Type:          "Info",
+		Title:         "通知",
+		Message:       str,
+		DefaultButton: "是",
+	})
 }
