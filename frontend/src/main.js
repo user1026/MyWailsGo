32a74e28411c5904935dac26dd95e66f3284b8bc
user1026 <2914153618@qq.com>
2023-04-22 23:43:26 +0800

    1

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,13 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from "@/router/index.js";
-//import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 
 
 const app = createApp(App)
-// for (const [key, Component] of Object.entries(ElementPlusIconsVue)) {
-//     app.component(key, Component)
-// }
+for (const [key, Component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, Component)
+}
 app.use(router)
 app.mount('#app')
--- frontend/src/views/index.vue
+++ frontend/src/views/index.vue
@@ -1,6 +1,67 @@
-<template>1231123</template>
+<template>
+    <div id="context">
+        <el-row :gutter="0">
+        <el-col :span="leftSpan">
+            <div id="left">
+                <div class="leftHeader">
+                    <el-icon @click="changeCollapse">
+                                <Menu />
+                    </el-icon>
+                </div>
+                    <el-menu
+                    default-active="2"
+                    class="leftMenu"
+                    :collapse="isCollapse"
+                    >
+                        <el-menu-item>图表</el-menu-item>
+                        <el-menu-item>硬件选择</el-menu-item>
+                        <el-menu-item></el-menu-item>
+                        <el-menu-item></el-menu-item>
+                        <el-menu-item></el-menu-item>
+                    </el-menu>
+            </div>
+        </el-col>
+        <el-col :span="rightSpan">
+            <div id="right">
+                    <el-button @click="t">+</el-button>
+            </div>
+        </el-col>
+    </el-row>
+    </div>
+</template>
 <script setup>
 import { ref, reactive, onMounted } from 'vue'
-
+const leftSpan=ref(4);
+const rightSpan=ref(20);
+const isCollapse=ref(true);
+const t=()=>{
+    leftSpan.value=8;
+    rightSpan.value=16
+}
+const changeCollapse=()=>{
+    isCollapse.value=!isCollapse.value
+}
 </script>
-<style lang='scss' scoped></style>
+<style lang='scss' scoped>
+#context{
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+}
+#left{
+    width: 100%;
+    height: 100vh;
+    background-color: #203561;
+    .leftMenu{
+        width: 100%;
+        background-color: #203561;
+        border-right: none;
+    }
+   
+}
+#right{
+    width: 100%;
+    height: 100vh;
+    background-color: blue;
+}
+</style>
