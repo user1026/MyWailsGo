7ee444d0dc2ca68861090b57777b9747206db6ba
user1026 <2914153618@qq.com>
2023-05-31 17:30:00 +0800

    1

--- frontend/src/views/computer/index.vue
+++ frontend/src/views/computer/index.vue
@@ -8,112 +8,12 @@ import { ref,reactive,onMounted } from 'vue'
 import tu from "@/views/echarts/index.vue"
 import {GetCpuInfo,GetUsingCpuInfo} from "../../../wailsjs/go/main/App.js"
 import * as echarts from "echarts"
+import CpuInfo from "./cpuInfo.js"
+const cpuinfo=CpuInfo();
 const cpuInfo=ref({})
 const UsingCpu=ref(0);
 const chart=ref(null)
-const option =ref({
-  title: [
-    {
-      text: "cpu使用率",
-      x: "center",
-      top: "55%",
-      textStyle: {
-        color: "#FFFFFF",
-        fontSize: 16,
-        fontWeight: "100",
-      },
-    },
-    {
-      text: `${UsingCpu.value}`,
-      x: "center",
-      y: "center",
-      textStyle: {
-      //  fontSize: "60",
-        color: "#FFFFFF",
-        fontFamily: "DINAlternate-Bold, DINAlternate",
-        foontWeight: "600",
-      },
-    },
-  ],
-  backgroundColor: "#111",
-  polar: {
-    radius: ["42%", "52%"],
-    center: ["50%", "50%"],
-  },
-  angleAxis: {
-    max: 100,
-    show: false,
-  },
-  radiusAxis: {
-    type: "category",
-    show: true,
-    axisLabel: {
-      show: false,
-    },
-    axisLine: {
-      show: false,
-    },
-    axisTick: {
-      show: false,
-    },
-  },
-  series: [
-    {
-      name: "",
-      type: "bar",
-      roundCap: true,
-      barWidth: 30,
-      showBackground: true,
-      backgroundStyle: {
-        color: "rgba(66, 66, 66, .3)",
-      },
-      data: [60],
-      coordinateSystem: "polar",
-      itemStyle: {
-        normal: {
-          color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [
-            {
-              offset: 0,
-              color: "#16CEB9",
-            },
-            {
-              offset: 1,
-              color: "#6648FF",
-            },
-          ]),
-        },
-      },
-    },
-    {
-      name: "",
-      type: "pie",
-      startAngle: 80,
-      radius: ["56%"],
-      hoverAnimation: false,
-      center: ["50%", "50%"],
-      itemStyle: {
-        color: "rgba(66, 66, 66, .1)",
-        borderWidth: 1,
-        borderColor: "#5269EE",
-      },
-      data: [100],
-    },
-    {
-      name: "",
-      type: "pie",
-      startAngle: 80,
-      radius: ["38%"],
-      hoverAnimation: false,
-      center: ["50%", "50%"],
-      itemStyle: {
-        color: "rgba(66, 66, 66, .1)",
-        borderWidth: 1,
-        borderColor: "#5269EE",
-      },
-      data: [100],
-    },
-  ],
-});
+
 
 onMounted(async ()=>{
   cpuInfo.value=await GetCpuInfo().then(res=>res);
